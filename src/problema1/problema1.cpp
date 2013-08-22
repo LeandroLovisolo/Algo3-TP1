a91eed26250063459e59135336a0714c1f4245dd
lucasr20 <lucasrossi20@gmail.com>
2013-08-22 00:40:42 -0300

    otra forma de resolver el ejercicio 1

--- src/problema1/problema1.cpp
+++ src/problema1/problema1.cpp
@@ -29,3 +29,72 @@ int problema1(int capacidad_camiones, const std::vector<int> &paquetes) {
 
 	return 0;
 }
+
+/* otra forma de resolver el ej1 con make_heap()
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include<algorithm>
+
+int limite;
+
+using namespace std;
+
+vector<int> leerDatos (istream& file) {
+    int peso;
+    int n;
+    file >> limite;
+    file >> n;
+    vector<int> res = vector<int>();
+
+    for (int i=0; i<n; i++) {
+        file >> peso;
+        res.push_back(peso);
+    }
+
+    return res;
+}
+
+bool ordenarCamiones(pair<int,int> x, pair<int,int> y){
+    return(x.second<y.second);
+}
+
+int main()
+{
+    string archivo = "prueba1.txt";
+    ifstream ifs (archivo.c_str(), ifstream::in);
+    if (!ifs.is_open()) {
+    cout << "no se pudo abrir el archivo " << archivo << endl;
+    return 0;
+    }
+    vector<int> paquetes = leerDatos(ifs);
+    vector<pair<int, int> > camiones = vector<pair<int, int> >();
+
+    make_heap(camiones.begin(),camiones.end());
+
+    for (int i = 0; i<paquetes.size(); i++) {
+        if(camiones.size() == 0) {
+            camiones.push_back(pair<int,int> (limite-paquetes[i],1));
+        }
+        else if(paquetes[i]<=camiones.front().first) {
+            camiones.front().first -= paquetes[i];
+        }
+        else {
+            camiones.push_back(pair<int,int> (limite - paquetes[i], camiones.size()+1));
+            push_heap(camiones.begin(),camiones.end());
+        }
+    }
+
+    cout << "cantidad de camiones " << camiones.size() << endl;
+
+    sort(camiones.begin(),camiones.end(),ordenarCamiones);
+
+    for (int i = 0; i<camiones.size(); i++){
+        cout << limite - camiones[i].first << " ";
+    }
+
+
+}
+
+*/
