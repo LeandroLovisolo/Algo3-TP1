a5bd7a135bb67c5342fc66aa4a81f027694c19be
Lautaro Petaccio <lausuper@gmail.com>
2013-08-22 23:28:38 -0300

    Lectura de datos problema 3

--- src/problema3/problema3_main.cpp
+++ src/problema3/problema3_main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
-
+#include "problema3.h"
+using namespace std;
 int main() {
+	int n;
+	if(cin.eof()) {
+		cout << "Error en el formato de entrada" << endl;
+		return 0;
+	}
+	cin >> n;
+	int m;
+	if(cin.eof()) {
+		cout << "Error en el formato de entrada" << endl;
+		return 0;
+	}
+	cin >> m;
+
+	Piso mapa(n,m);
+	for (int i = 0; i < n; ++i) {
+		for(int j = 0; j < m; ++j) {
+			if(!cin.eof())	cin >> mapa.en(i, j);
+		}
+	}
 	return 0;
 }
