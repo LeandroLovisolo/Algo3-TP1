a15c7a62bde245cd5d5c5a63e42726f7f861f1ec
lucasr20 <lucasrossi20@gmail.com>
2013-08-24 17:51:31 -0300

    performance problema1

--- src/problema1/problema1_tests.cpp
+++ src/problema1/problema1_tests.cpp
@@ -79,12 +79,12 @@ TEST(problema1, PaquetesSeAgreganADistintosCamiones) {
 	delete res;	
 }
 
-double performanceTest(const int iteraciones, const int limite, const vector<int> &paquetes) {
+double performance(int iteraciones, int limite, vector<int> paquetes) {
 	clock_t start = clock();
 	double time = 0;
-	for(int j=0;j<iteraciones;j++) {
-		clock_t end = clock();
+	for (int i = 0; i < iteraciones; ++i) {
 		problema1(limite, paquetes);
+		clock_t end = clock();
 		time += (double) (end-start) / CLOCKS_PER_SEC * 1000.0;
 	}
 	return time/iteraciones;
