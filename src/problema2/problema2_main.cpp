d71394d89be986bf2d53ae35737d61f033d3c99f
Leandro Lovisolo <leandro@leandro.me>
2013-08-28 10:28:17 -0300

    Implementación problema 2

--- src/problema2/problema2_main.cpp
+++ src/problema2/problema2_main.cpp
@@ -1,39 +1,40 @@
 #include <iostream>
+
 #include "problema2.h"
+
 using namespace std;
 
 int main() {
-	//Main para la lectura
-	int n;
-	if(cin.eof()) {
-		cout << "Error en el formato de entrada" << endl;
-		return 0;
-	}
-	cin >> n;
-	//Construyo un vector de pairs de n elementos en 0
-	vector<Curso> cursos(n, Curso(0,0,0));
-	int i=0;
-	while(!cin.eof() && i < n) {
-		//Cargo fecha de inicio y de finalización
-		cin >> cursos[i]._inicio;
-		cin >> cursos[i]._final;
-		cursos[i]._numero = i+1;
-		i++;
-	}
-	vector<Curso>* resultado = problema2(cursos);
-	/* Funcion para ver si fueron agregados correctamente y ordenados para probar el sort
-	for(unsigned int j=0;j<cursos.size();j++) {
-		cout << cursos[j]._numero << ": [" << cursos[j]._inicio << "," << cursos[j]._final << "] ";
-	}
-	cout << endl;
-	*/
+    while(true) {
+        vector<Curso> cursos;
 
-	/* Imprimo el numero del curso
-	for(i=0;i<resultado.size();i++) {
-		cout << resuldato[i]._numero << " ";
-	}
-	cout << endl;
-	*/
-	delete resultado;
-	return 0;
+        // Leo el tamaño del problema.
+        int n;
+        cin >> n;
+
+        // Termino el ciclo si la lectura anterior fracasó.
+        if(cin.eof()) break;
+
+        // Leo n cursos.
+        for(int i = 1; i <= n; i++) {
+            int inicio, fin;
+            cin >> inicio;
+            cin >> fin;
+            cursos.push_back(Curso(inicio, fin, i));
+        }
+
+        // Ignoro el resto de la línea (símbolo # y comentarios posteriores.)
+        string s;
+        getline(cin, s);
+
+        // Resuelvo el problema
+        vector<Curso> solucion = problema2(cursos);
+
+        // Imprimo los índices de los cursos de la solución hallada.
+        for(size_t i = 0; i < solucion.size(); i++)
+            cout << indice(solucion[i]) << " ";
+        cout << endl;
+    }
+
+    return 0;
 }
